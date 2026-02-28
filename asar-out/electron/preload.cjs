0dbf2e205436bb901752c103fc82547fb4c26895
xander1235 <rathodveerender25@gmail.com>
2026-02-28 19:02:59 +0530

    chore: remove debug and temp files

--- asar-out/electron/preload.cjs
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("api", {
-  ping: () => ipcRenderer.invoke("app:ping"),
-  sendRequest: (payload) => ipcRenderer.invoke("http:sendRequest", payload),
-  saveState: (key, value) => ipcRenderer.invoke("db:saveState", key, value),
-  loadState: (key) => ipcRenderer.invoke("db:loadState", key)
-});
